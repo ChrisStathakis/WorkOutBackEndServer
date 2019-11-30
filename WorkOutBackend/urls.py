791e9ee97dfacb3bb6694b3e9f531eeca49b7503
Zefarak <lirageika@hotmail.gr>
2019-11-30 15:31:18 +0200

    phg

--- WorkOutBackend/urls.py
+++ WorkOutBackend/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from frontend.views import homepage_api
 
 urlpatterns = [
